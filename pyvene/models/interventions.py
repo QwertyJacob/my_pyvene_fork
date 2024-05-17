719621c5d212eb42572cf012c4431232dfe0e983
Jesus Cevallos <jesusfcevallos@gmail.com>
2024-05-17 09:16:08 +0000

    collectiveInterventionKLD

--- pyvene/models/interventions.py
+++ pyvene/models/interventions.py
@@ -174,26 +174,15 @@ class CollectIntervention(ConstantSourceIntervention):
     def __str__(self):
         return f"CollectIntervention()"
         
-class CollectInterventionKLD(ConstantSourceIntervention):
+class CollectInterventionKLD(CollectIntervention):
 
     """Collect activations."""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs, keep_last_dim=True)
-        
-    def forward(self, base, source=None, subspaces=None):
-        return _do_intervention_by_swap(
-            base,
-            source,
-            "collect",
-            self.interchange_dim,
-            subspaces,
-            subspace_partition=self.subspace_partition,
-            use_fast=self.use_fast,
-        )
 
     def __str__(self):
-        return f"CollectIntervention()"
+        return f"CollectInterventionKLD()"
 
 class SkipIntervention(BasisAgnosticIntervention, LocalistRepresentationIntervention):
 
